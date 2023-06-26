0181a219b58e6adcb9c69b77dd81a5aab2a0abb3
Musadiq Aliyev <musadiq.aliyev@gmail.com>
2023-06-27 02:07:18 +0400

    bundle_query

---
+++ code/bundle_query_1.sql
@@ -0,0 +1,11 @@
+select c_no,
+transaction_id,
+merchant_category_code,
+transaction_date,
+merchant_catergory_description,
+amoiunt,
+card_name
+from xxx.transaction_table
+where upper(substr(transaction_type,0,2)) in ('xx')
+and transaction_direction = 'xx'
+and transaction_date between to_date('01-05-2023', 'dd-mm-yyyy') and to_date('31-05-2023', 'dd-mm-yyyy')
